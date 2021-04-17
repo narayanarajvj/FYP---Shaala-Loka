81d0a62bf5dd962201cc96410b492dd413decdd3
Narayanaraj Govindaraju <48882141+narayanarajvj@users.noreply.github.com>
2021-04-17 23:19:09 +0530

    Add files via upload

---
+++ anagha-flask/MaterialStorage.py
@@ -0,0 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
+
+cred = credentials.Certificate("shaala-loka-firebase-adminsdk-xsp4x-5eea3da522.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket': '<BUCKET-ID>.appspot.com'
+})
+
+bucket = storage.bucket()
+
+classname = 'aca'
+org_id = '1JS'
+filename = 'vj4.jpg'
+
+blob = bucket.blob(org_id + '/' + classname + '/' + filename)
+# blob.download_to_filename(org_id + '/' + classname + '/' + filename)
+# blob.upload_from_filename(filename)
+blob.make_public()
+print(blob.public_url)
